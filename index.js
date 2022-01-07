9e233d3b6f9c305a64dcaf077e66e006065ea2e0
Kaho Pong <pongkaho@gmail.com>
2022-01-08 03:51:53 +0800

    kaho

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const port = 8000;
+const port = 8080;
 
 //require classes
 //================
 
-const BookService = require("./services/BookService.js");
-const DisplayService = require("./services/DisplayService.js");
-const FavService = require("./services/FavService.js");
-const HostService = require("./services/HostService.js");
-const ImageService = require("./services/ImageService.js");
-const InfoService = require("./services/InfoService.js");
-const MyCourseService = require("./services/MyCourseService.js");
+const FormService = require("./services/FormService.js");
 
-const BookRouter = require("./routers/BookRouter.js");
-const DisplayRouter = require("./routers/DisplayRouter.js");
-const FavRouter = require("./routers/FavRouter.js");
-const HostRouter = require("./routers/HostRouter.js");
-const ImageRouter = require("./routers/ImageRouter.js");
-const InfoRouter = require("./routers/InfoRouter.js");
-const MyCourseRouter = require("./routers/MyCourseRouter.js");
+const FormRouter = require("./routers/FormRouter.js");
 const ViewRouter = require("./routers/ViewRouter.js");
 
 //Configure knex
@@ -28,18 +16,13 @@ const knex = require("knex")(knexFile);
 //Configure express, handlebars
 //=============================
 const express = require("express");
-const expressFileUpload = require("express-fileupload");
-const fs = require("fs");
-const path = require("path");
 const { engine } = require("express-handlebars");
 const flash = require("connect-flash");
 const session = require("express-session");
-const passport = require("passport");
 
 const app = express();
-app.use(expressFileUpload());
 app.set("view engine", "handlebars");
-app.engine("handlebars", engine({ defaultLayout: "shop_main" }));
+app.engine("handlebars", engine({ defaultLayout: "rooftop_main" }));
 
 require("dotenv").config();
 app.use(express.json());
@@ -54,41 +37,19 @@ app.use(
 app.use(flash());
 
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/uploaded"));
-app.use(passport.initialize());
-app.use(passport.session());
-const cors = require("cors");
 
-// // Passport Authentication
-// //==================================
-// const passportJs = require("./Authentication/passport");
 
 // Set up Server and Routers
 //==================================
-const bookService = new BookService(knex);
-const displayService = new DisplayService(knex);
-const favService = new FavService(knex);
-const hostService = new HostService(knex);
-const uploadPath = __dirname + "/uploaded";
-const imageService = new ImageService(uploadPath, fs, path);
-const infoService = new InfoService(knex);
-const myCourseService = new MyCourseService(knex);
+const formService = new FormService(knex);
 
-app.use("/book", new BookRouter(bookService, express).router());
-app.use("/display", new DisplayRouter(displayService, express).router());
-app.use("/fav", new FavRouter(favService, express).router());
-app.use("/host", new HostRouter(hostService, express).router());
-app.use("/image", new ImageRouter(imageService, express).router());
-app.use("/info", new InfoRouter(infoService, express).router());
-app.use("/myCourse", new MyCourseRouter(myCourseService, express).router());
-app.use("/", new ViewRouter(passport, express).router());
+app.use("/form", new FormRouter(formService, express).router());
+app.use("/", new ViewRouter(express).router());
 
 app.get("/", (req, res) => {
-  res.render("usershb/index", { layout: "login_main" });
+  res.render("modal");
 });
-// app.get("/index/course", (req, res) => {
-//     res.render("usershb/courseinfo.handlebars", { layout: "login_main" });
-// });
+
 // Listen to port
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
--- routers/ViewRouter.js
+++ routers/ViewRouter.js
@@ -2,181 +2,26 @@
 // //================================
 // const auth = require("../Authentication/auth");
 class ViewRouter {
-    constructor(passport, express) {
-        this.passport = passport;
+    constructor(express) {
         this.express = express;
     }
 
     router() {
         let router = this.express.Router();
 
-        //Login Signup handlebars
-
-        router.get("/login", (req, res) => {
-            res.render("login/login", {
-                error: req.flash("error"),
-                layout: "login_main",
-            });
-        });
-
-        router.get("/signup", (req, res) => {
-            res.render("login/signup", { layout: "login_main" });
-        });
-
-        router.get("/shopsignup", (req, res) => {
-            res.render("login/shopsignup", { layout: "login_main" });
-        });
-
+        //Error Page
         router.get("/error", (req, res) => {
             res.render("error", {
                 error: req.flash("error"),
             });
         });
 
-        // router.get('/index', auth.isLoggedIn, (req, res) => {
-        //     console.log(`${req.session.passport.user.username} logged in`)
-        //     res.render('usershb/index', {
-        //         user: req.session.passport.user.username,
-        //         layout: 'users_main'
-        //     })
-        // })
-        router.get("/index", (req, res) => {
-
-            if (
-                req.session.passport != undefined &&
-                req.session.passport.user.isUser == true
-            ) {
-                // console.log("My user is", req.session.passport.user)
-                res.render("usershb/index", {
-                    layout: "users_main",
-                });
-            } else {
-                res.render("usershb/index", {
-                    layout: "login_main",
-                    user_id: 'Guest',
-                });
-            }
-        });
-
-        router.get("/index/course", (req, res) => {
-            if (req.session.passport != undefined && Object.keys(req.session.passport).length != 0) {
-                if (req.session.passport.user.isUser == true) {
-                    res.render("usershb/courseinfo", {
-                        layout: "users_main",
-                    });
-                }
-            } else {
-                res.render("usershb/courseinfo", {
-                    layout: "login_main",
-                });
-            }
-
-
-        });
-        router.get("/logout", function(req, res) {
-            req.logout();
-            res.redirect("/login");
-        });
-
-        // router.post(
-        //     "/signup",
-        //     this.passport.authenticate("local-signup", {
-        //         successFlash: true,
-        //         successRedirect: "/login",
-        //         failureRedirect: "/error",
-        //         failureFlash: true,
-        //     })
-        // );
-
-        // router.post(
-        //     "/shopsignup",
-        //     this.passport.authenticate("local-signup2", {
-        //         successFlash: true,
-        //         successRedirect: "/login",
-        //         failureRedirect: "/error",
-        //         failureFlash: true,
-        //     })
-        // );
-
-        // router.post(
-        //     "/login",
-        //     this.passport.authenticate("local-login", {
-        //         successRedirect: "/index",
-        //         failureRedirect: "/login",
-        //         failureFlash: true,
-        //     })
-        // );
-
-        // let passportCopy = this.passport;
-        // router.post("/login", function(req, res, next) {
-        //     passportCopy.authenticate("local-login", function(err, user, info) {
-        //         if (err) {
-        //             return next(err);
-        //         }
-        //         if (!user) {
-        //             return res.redirect("/login");
-        //         }
-        //         req.logIn(user, function(err) {
-        //             if (err) {
-        //                 return next(err);
-        //             }
-        //             if (user.isUser == true) {
-        //                 return res.redirect("/index");
-        //             } else {
-        //                 return res.redirect("/dashboard");
-        //             }
-        //         });
-        //     })(req, res, next);
-        // });
-
-        router.get("/logout", (req, res) => {
-            console.log("logging out");
-            req.logout();
-            res.redirect("/");
-        });
-
-        //Shop side handlebars (default)
-        router.get("/dashboard", (req, res) => {
-            // console.log("passportDB", req.session.passport.user.shop_id);
-            res.render("shophb/dashboard", {
-                name: "Fanki",
-                shop_id: req.session.passport.user.shop_id,
-            });
-        });
-
-        router.get("/edit_shop_info", (req, res) => {
-            res.render("shophb/edit_shop_info"), { name: "Fanki" };
-        });
-
-        router.get("/add_course", (req, res) => {
-            res.render("shophb/add_course"), { name: "Fanki" };
-        });
-
-        router.get("/edit_course", (req, res) => {
-            res.render("shophb/edit_course"), { name: "Fanki" };
-        });
-
-        router.get("/list_booking", (req, res) => {
-            res.render("shophb/list_booking"), { name: "Fanki" };
-        });
-
-        router.get("/upload_pic", (req, res) => {
-            res.render("shophb/upload_pic"), { name: "Fanki" };
+        router.get("/sections", (req, res) => {
+            res.render("sections");
         });
 
 
-        //User side handlebars
-        // res.render('__FILL_ME_IN__', { layout: 'users_main' })
-        router.get("/mycourse", (req, res) => {
-            res.render("usershb/mycourse", { name: "Fanki", layout: "users_main" });
-        });
 
-        router.get("/edit_member_info", (req, res) => {
-            res.render("usershb/edit_member_info", {
-                name: "Fanki",
-                layout: "users_main",
-            });
-        });
 
         return router;
     }
